b68a8998b651f2dc441adabae6d5023e067a6bf6
RetupK <retupk@gmail.com>
2020-08-06 18:59:46 +0200

    Updates

---
+++ precache-manifest.766c5f572a9fdad82aea4181f417e728.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c504bce24620b3df28ebc7b04358f0",
+    "url": "/Maps/index.html"
+  },
+  {
+    "revision": "60492886c0a2b9da0a31",
+    "url": "/Maps/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "3d5efdd79bc0d061619f",
+    "url": "/Maps/static/css/main.e9ed7a2e.chunk.css"
+  },
+  {
+    "revision": "60492886c0a2b9da0a31",
+    "url": "/Maps/static/js/2.3017e346.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Maps/static/js/2.3017e346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d5efdd79bc0d061619f",
+    "url": "/Maps/static/js/main.eb60e904.chunk.js"
+  },
+  {
+    "revision": "809c1d822f06d1139877",
+    "url": "/Maps/static/js/runtime-main.02b39ff3.js"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/Maps/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/Maps/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/Maps/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/Maps/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
